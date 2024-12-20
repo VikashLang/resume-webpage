d648eac1ed4dc5fd03c82a3bde09c5d20259aeab
VikashLang <vikashlang03@gmail.com>
2024-12-20 09:56:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const navLinks = document.querySelectorAll("header nav ul li a");
+    navLinks.forEach(link => {
+      link.addEventListener("mouseover", () => {
+        link.style.transform = "scale(1.1)";
+      });
+      link.addEventListener("mouseout", () => {
+        link.style.transform = "scale(1)";
+      });
+    });
+  });
+  
